bad164225188e4051ddd06ca8243547570aa4302
Brad Willis <brad.willis@outlook.com>
2018-06-09 22:52:06 -0400

    Initial Project

---
+++ PictureSort/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input source folder:");
+            string sourceFolder = Console.ReadLine();
+
+            Console.WriteLine("Input target folder:");
+            string targetFolder = Console.ReadLine();
+
+            Sorter sorter = new Sorter(sourceFolder, targetFolder);
+            sorter.sort();
+        }
+    }
+}
---
+++ PictureSort/Sorter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureSort
+{
+    class Sorter
+    {
+        private string m_sourceFolder;
+        private string m_targetFolder;
+
+        public Sorter(string sourceFolder, string targetFolder)
+        {
+            m_sourceFolder = sourceFolder;
+            m_targetFolder = targetFolder;
+        }
+
+        public void sort()
+        {
+            if (!Directory.Exists(m_sourceFolder))
+            {
+                throw new Exception("Invalid source folder");
+            }
+
+            DirectoryInfo sourceFolder = new DirectoryInfo(m_sourceFolder);
+            IEnumerable<FileInfo> sourceFiles = sourceFolder.EnumerateFiles();
+
+            foreach (var file in sourceFiles)
+            {
+                if ((file.Attributes & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    continue;
+                }
+
+
+            }
+
+        }
+    }
+}
